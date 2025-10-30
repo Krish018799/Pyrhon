c760213cb6407a879c0fc524371715fbde7a91eb
Krish018799 <krishmistry612@gmail.com>
2025-10-30 10:14:22 +0530

    Add files via upload

---
+++ Chapter 9/ASCII in python.py
@@ -0,0 +1,8 @@
+# In Python, you can get the ASCII value of a character using the built-in ord() function, and convert an ASCII value back to a character using the chr() function.
+print(ord('A'))  # Output: 65
+print(ord('a'))  # Output: 97
+print(ord('0'))  # Output: 48
+
+print(chr(65))  # Output: 'A'
+print(chr(97))  # Output: 'a'
+print(chr(48))  # Output: '0'
